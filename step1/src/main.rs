fb3cedea3d068aede95f65bee88f217d96940265
choigunwoo <gunwoo8873@outlook.kr>
2024-07-21 18:10:48 +0900

    Main build file

--- step1/src/main.rs
+++ step1/src/main.rs
@@ -8,19 +8,15 @@ fn main() {
     println!("Test Number");
     println!("Please input your guess");
 
-    /*
-    * let : 변수의 값을 바인딩(bind) 한다.
-    * mut : 변경 가능한 변수 및 참조 또는 포인터
-    * :: : new가 String 타입의 연관 함수(associated function)임을 나타냄.
-    */
+    // let : 변수의 값을 바인딩(bind) 한다.
+    // mut : 변경 가능한 변수 및 참조 또는 포인터
+    // :: : new가 String 타입의 연관 함수(associated function)임을 나타냄.
     let mut guess = String::new();
 
-    /*
-    * & : 데이터를 메모리로 여러 번 복사하지 않고 접근하기 위한 참조자(Reference)이다.
-    * &mut guess처럼 가변으로 만들 필요가 있다.
-    * io::stdin : 입력값을 받아 처리하는 과정 
-    * => stdin 함수는 Terminal의 handle을 타나내는 타입의 인스턴스를 돌려준다.
-    */
+    // & : 데이터를 메모리로 여러 번 복사하지 않고 접근하기 위한 참조자(Reference)이다.
+    // &mut guess처럼 가변으로 만들 필요가 있다.
+    // io::stdin : 입력값을 받아 처리하는 과정 
+    // => stdin 함수는 Terminal의 handle을 타나내는 타입의 인스턴스를 돌려준다.
     io::stdin()
         .read_line(&mut guess) // size = 8, align = 0x8
         .expect("Failed to read line");
