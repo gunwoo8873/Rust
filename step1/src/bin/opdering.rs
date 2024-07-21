cc59220aa08c926d7bbecff3d345580fee62e8ae
choigunwoo <gunwoo8873@outlook.kr>
2024-07-21 18:10:24 +0900

    Opdering Code update

--- step1/src/bin/opdering.rs
+++ step1/src/bin/opdering.rs
@@ -2,7 +2,38 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 
-fn main() {
+fn loop_opdering() {
+    let gen_num = rand :: thread_rng().gen_range(0..=1000);
+    
+    //// infinity try loop
+    loop {
+        //// select_num라는 변수를 정의
+        let mut select_num = String :: new();
+
+        io::stdin()
+            .read_line(&mut select_num) // size = 8, align = 0x8
+            .expect("Failed to read line");
+
+        //// let select_num: u32 = select_num.trim().parse().expect("Please type a Num");
+        let select_num: u32 = match select_num.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+            
+        match select_num.cmp(&gen_num) {
+            //// Ordering : 열거형 Less, Greater, Equal의 3가지 배리언트들을 가지고 있다.
+            Ordering :: Less => println!("Too small!"), // a < b
+            Ordering :: Greater => println!("Too big!"), // a > b
+            //Ordering :: Equal => println!("You win!"), // a = b
+            Ordering :: Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
+
+fn opdering() {
     let gen_num = rand :: thread_rng().gen_range(0..=100);
     let mut select_num = String::new(); // select_num라는 변수를 정의
     
@@ -11,8 +42,8 @@ fn main() {
         .expect("Failed to read line");
     println!("secret Number : {gen_num}");
 
-    // parse : 문자열을 다른 타입으로 변경 ex) String -> Number
-    // 주의 : 에러가 발생하기 쉽다. ex) A%같은 특수 문자열이 포함되어 있다면 정수로 바꿀 방법이 없다.
+    //// parse : 문자열을 다른 타입으로 변경 ex) String -> Number
+    //// 주의 : 에러가 발생하기 쉽다. ex) A%같은 특수 문자열이 포함되어 있다면 정수로 바꿀 방법이 없다.
     let select_num: u32 = select_num.trim().parse().expect("Please type a Num");
 
     println!("You select_num: {select_num}");
@@ -25,4 +56,9 @@ fn main() {
         Ordering :: Greater => println!("Too big!"), // a > b
         Ordering :: Equal => println!("You win!"), // a = b
     }
+}
+
+fn main() {
+    loop_opdering();
+    opdering();
 }
