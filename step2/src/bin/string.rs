2251e25e01a3b4868e2bc190664a4830012b92ba
choigunwoo <gunwoo8873@outlook.kr>
2024-08-02 21:35:52 +0900

    Move code to string.rs -> ownership

--- step2/src/bin/string.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    string_type();
-}
-
-fn string_type() {
-    let mut str = String :: from("STR Variable");
-    str.push_str(" + NEW add STR Variable");
-    println!("{}", str);
-}
